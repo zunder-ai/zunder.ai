0160fb3313fa153cb5d3bff7d52ca4c4c357d550
regenrek <hello@regenrek.at>
2024-08-21 04:36:53 +0200

    add tutorial urls into sitemap.xml

--- docs/server/api/__sitemap__/urls.ts
+++ docs/server/api/__sitemap__/urls.ts
@@ -2,11 +2,17 @@ import { defineEventHandler } from 'h3'
 import { serverQueryContent } from '#content/server'
 
 export default defineEventHandler(async (event) => {
-  const docs = await serverQueryContent(event).where({ _path: /^\/docs/ }).find()
-  return docs
-    .filter(doc => !doc._path.endsWith('_dir'))
-    .map(doc => ({
-      loc: doc._path,
-      lastmod: doc.updatedAt
+  const [docs, tutorials] = await Promise.all([
+    serverQueryContent(event).where({ _path: /^\/docs/ }).find(),
+    serverQueryContent(event).where({ _path: /^\/tutorials/ }).find()
+  ])
+
+  const allContent = [...docs, ...tutorials]
+
+  return allContent
+    .filter(item => !item._path.endsWith('_dir'))
+    .map(item => ({
+      loc: item._path,
+      lastmod: item.updatedAt
     }))
 })
